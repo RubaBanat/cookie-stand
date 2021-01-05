882d30e9c12d12672d40419b9c0b17998900a1f5
rubabanat <rubabanat1996@gmail.com>
2021-01-05 13:18:02 +0200

    total

--- JS/app.js
+++ JS/app.js
@@ -1,161 +1,176 @@
-"use strict";
-var Hours = ['6am', '7am', '8am', '9am', '10am', '11am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
-var shop = [{
-    location: 'Seattle', minCust: 23, maxCust: 65, average: 6.3, totalCooliesPerHour: [],
-    getRandominCusttInclusive: function () {
-        var minCustCust = Math.ceil(this.minCust);
-        var maxCustCust = Math.floor(this.maxCust);
-        return Math.floor(Math.random() * (maxCustCust - minCustCust + 1) + minCustCust);
-    }, totalCookies: function () {
-       var ul = document.createElement('ul');
-       var h3 = document.createElement('h3');
-        h3.textContent = this.location;
-        var total = 0;
-        for (var i = 0; i < Hours.length; i++) {
-            var value = this.getRandominCusttInclusive() * this.average;
-            total = total + value;
-            this.totalCooliesPerHour.push(value);
-            renderTheList(ul, value, Hours[i])
-        }
-        var totalLi = document.createElement('li');
-        totalLi.innerHTML = `Total: ${total}`
-        var main = document.getElementById('main')
-        main.appendChild(h3);
-        main.appendChild(ul)
-    }
-}, {
-    location: 'Tokyo', minCust: 3, maxCust: 24, average: 1.2, totalCooliesPerHour: [],
-    getRandominCusttInclusive: function () {
-        var minCust = Math.ceil(this.minCust);
-        var maxCust = Math.floor(this.maxCust);
-        return Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
-    }, totalCookies: function () {
-        var ul = document.createElement('ul');
-        var h3 = document.createElement('h3');
-        var info = document.write("")
-        h3.textContent = this.location;
-        var total = 0;
-        for (var i = 0; i < Hours.length; i++) {
-            var value = this.getRandominCusttInclusive() * this.average;
-            total = total + value;
-            this.totalCooliesPerHour.push(value);
-            renderTheList(ul, value, Hours[i])
-        }
-        var totalLi = document.createElement('li');
-        totalLi.innerHTML = `Total: ${total}`
-        var main = document.getElementById('main')
-        main.appendChild(h3);
-        main.appendChild(ul)
-    }
-}, {
-    location: 'Dubai', minCust: 11, maxCust: 38, average: 3.7, totalCooliesPerHour: [],
-    getRandominCusttInclusive: function () {
-        var minCust = Math.ceil(this.minCust);
-       var maxCust = Math.floor(this.maxCust);
-        return Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
-    }, totalCookies: function () {
-        var ul = document.createElement('ul');
-        var h3 = document.createElement('h3');
-        h3.textContent = this.location;
-        var total = 0;
-        for (var i = 0; i < Hours.length; i++) {
-            var value = this.getRandominCusttInclusive() * this.average;
-            total = total + value;
-            this.totalCooliesPerHour.push(value);
-            renderTheList(ul, value, Hours[i])
-        }
-        var totalLi = document.createElement('li');
-        totalLi.innerHTML = `Total: ${total}`
-        var main = document.getElementById('main')
-        main.appendChild(h3);
-        main.appendChild(ul)
-    }
-}, {
-    location: 'Paris', minCust: 20, maxCust: 38, average: 2.3, cookiesAtHour: [],
-    random: function () {
-        var minCust = Math.ceil(this.minCust);
-        var maxCust = Math.floor(this.maxCust);
-        return Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
-    }, totalCookies: function () {
-        var ul = document.createElement('ul');
-        var h3 = document.createElement('h3');
-        h3.textContent = this.location;
-        let total = 0;
-        for (var i = 0; i < Hours.length; i++) {
-            var value = this.random() * this.average;
-            total = total + value;
-            this.cookiesAtHour.push(value);
-            renderTheList(ul, value, Hours[i])
-        }
-        var totalLi = document.createElement('li');
-        totalLi.innerHTML = `Total: ${total}`
-        var main = document.getElementById('main')
-        main.appendChild(h3);
-        main.appendChild(ul)
-    }
-}, {
-    location: 'Lima', minCust: 2, maxCust: 16, average: 4.6, totalCooliesPerHour: [],
-    getRandominCusttInclusive: function () {
-        var minCust = Math.ceil(this.minCust);
-        var maxCust = Math.floor(this.maxCust);
-        return Math.floor(Math.random() * (maxCust - minCust + 1) + minCust);
-    }, totalCookies: function () {
-        var ul = document.createElement('ul');
-        var h3 = document.createElement('h3');
-        h3.textContent = this.location;
-        let total = 0;
-        for (var i = 0; i < Hours.length; i++) {
-            var value = this.getRandominCusttInclusive() * this.average;
-            total = total + value;
-            this.totalCooliesPerHour.push(value);
-            renderTheList(ul, value, Hours[i])
-        }
-        var totalLi = document.createElement('li');
-        totalLi.innerHTML = `Total: ${total}`
-        var main = document.getElementById('main')
-        main.appendChild(h3);
-        main.appendChild(ul)
-    }
-}]
+'use strict'
 
-function renderTheList(ul, value, time) {
-    var li = document.createElement('li');
-    li.innerText = `${time}: ${Math.round(value)} cookies`;
-    ul.appendChild(li);
-}
-function echo() {
-    var table = document.getElementById('table');
-    var infoTabel = document.createElement('table')
-    var thLocation = document.createElement('th');
-    thLocation.innerHTML = 'city';
-    infoTabel.appendChild(thLocation);
-    var thminCust = document.createElement('th');
-    thminCust.innerHTML = 'minimum';
-    infoTabel.appendChild(thminCust);
-    var thmaxCust = document.createElement('th');
-    thmaxCust.innerHTML = 'maximum';
-    infoTabel.appendChild(thmaxCust);
-    var thAvg = document.createElement('th');
-    thAvg.innerText = 'Avg';
-    infoTabel.appendChild(thAvg);
+var storeOne = {
+    storeLocation: "Seattle",
+    minCusts: 23,
+    maxCusts: 65,
+    averCookiesCust: 6.3,
+    arrCustNumber: [],
+    total: 0,
+    hourlySales: [],
+    numberOfCusts: function (min, max) {
+        for (var i = 0; i < 14; i++) {
+            storeOne.arrCustNumber[i] = Math.floor(Math.random() * (max - min) + min);
+        }   
+    },
+    calculateSales: function () {
 
-    for (var i = 0; i < shop.length; i++) {
-        shop[i].totalCookies();
-        var row = document.createElement('tr')
-        var location = document.createElement('td')
-        location.innerHTML = shop[i].location;
-        row.appendChild(location);
-        var minCust = document.createElement('td');
-        minCust.innerHTML = shop[i].minCust;
-        row.appendChild(minCust);
-       var maxCust = document.createElement('td');
-        maxCust.innerHTML = shop[i].maxCust;
-        row.appendChild(maxCust);
-        var avg = document.createElement('td');
-        avg.innerText = shop[i].average;
-        row.appendChild(avg);
-        infoTabel.appendChild(row);
-    };
-    table.appendChild(infoTabel)
-}
-echo();
+        for (var i = 0; i < this.arrCustNumber.length; i++) {
+            storeOne.hourlySales[i] = Math.floor(storeOne.arrCustNumber[i] * this.averCookiesCust);
+        }
+    },
+    calculateTotal: function () {
+
+        for (var i = 0; i < this.hourlySales.length; i++) {
+            this.total = this.total + storeOne.hourlySales[i];
+        }
+    }
+};
+storeOne.numberOfCusts(storeOne.minCusts,storeOne.maxCusts);
+storeOne.calculateSales();
+storeOne.calculateTotal();
+var storeTwo = {
+    storeLocation: "Tokyo",
+    minCusts: 3,
+    maxCusts: 24,
+    averCookiesCust: 1.2,
+    arrCustNumber: [],
+    hourlySales: [],
+    total: 0,
+    numberOfCusts: function (min, max) {
+        for (var i = 0; i < 14; i++) {
+            storeTwo.arrCustNumber[i] = Math.floor(Math.random() * (max - min) + min);
+        }
+
+    },
+    calculateSales: function () {
+
+        for (var i = 0; i < this.arrCustNumber.length; i++) {
+            storeTwo.hourlySales[i] = Math.floor(storeTwo.arrCustNumber[i] * this.averCookiesCust);
+        }
+    },
+    calculateTotal: function () {
+
+        for (var i = 0; i < this.hourlySales.length; i++) {
+            this.total = this.total + storeTwo.hourlySales[i];
+        }
+    }
+};
+storeTwo.numberOfCusts(storeTwo.minCusts,storeTwo.maxCusts);
+storeTwo.calculateSales();
+storeTwo.calculateTotal();
+var storeThree = {
+    storeLocation: "Dubai",
+    minCusts: 11,
+    maxCusts: 28,
+    averCookiesCust: 3.7,
+    arrCustNumber: [],
+    hourlySales: [],
+    total: 0,
+    numberOfCusts: function (min, max) {
+        for (var i = 0; i < 14; i++) {
+            storeThree.arrCustNumber[i] = Math.floor(Math.random() * (max - min) + min);
+        }
+
+    },
+    calculateSales: function () {
+
+        for (var i = 0; i < this.arrCustNumber.length; i++) {
+            storeThree.hourlySales[i] = Math.floor(storeThree.arrCustNumber[i] * this.averCookiesCust);
+        }
+    },
+    calculateTotal: function () {
+
+        for (var i = 0; i < this.hourlySales.length; i++) {
+            this.total = this.total + storeThree.hourlySales[i];
+        }
+    }
+
+};
+storeThree.numberOfCusts(storeThree.minCusts,storeThree.maxCusts);
+storeThree.calculateSales();
+storeThree.calculateTotal();
+var storeFour = {
+    storeLocation: "Paris",
+    minCusts: 20,
+    maxCusts: 38,
+    averCookiesCust: 2.3,
+    arrCustNumber: [],
+    hourlySales: [],
+    total: 0,
+    numberOfCusts: function (min, max) {
+        for (var i = 0; i < 14; i++) {
+            storeFour.arrCustNumber[i] = Math.floor(Math.random() * (max - min) + min);
+        }
+
+    },
+    calculateSales: function () {
+
+        for (var i = 0; i < this.arrCustNumber.length; i++) {
+            storeFour.hourlySales[i] = Math.floor(storeFour.arrCustNumber[i] * this.averCookiesCust);
+        }
+    },
+    calculateTotal: function () {
+
+        for (var i = 0; i < this.hourlySales.length; i++) {
+            this.total = this.total + storeFour.hourlySales[i];
+        }
+    }
+};
+storeFour.numberOfCusts(storeFour.minCusts,storeFour.maxCusts);
+storeFour.calculateSales();
+storeFour.calculateTotal();
+var storeFive = {
+    storeLocation: "Lima",
+    minCusts: 2,
+    maxCusts: 16,
+    averCookiesCust: 4.6,
+    arrCustNumber: [],
+    hourlySales: [],
+    total: 0,
+    numberOfCusts: function (min, max) {
+        for (var i = 0; i < 14; i++) {
+            storeFive.arrCustNumber[i] = Math.floor(Math.random() * (max - min) + min);
+        }
+
+    },
+    calculateSales: function () {
+
+        for (var i = 0; i < this.arrCustNumber.length; i++) {
+            storeFive.hourlySales[i] = Math.floor(storeFive.arrCustNumber[i] * this.averCookiesCust);
+            console.log(this.hourlySales);
+
+        }
+    },
+    calculateTotal: function () {
+
+        for (var i = 0; i < this.hourlySales.length; i++) {
+            this.total = this.total + storeFive.hourlySales[i];
+        }
+    }
+};
+storeFive.numberOfCusts(storeFive.minCusts,storeFive.maxCusts);
+storeFive.calculateSales();
+storeFive.calculateTotal();
+var workingHours = ['6am:','7am: ','8am: ','9am: ','10am: ','11am: ','12pm: ','1pm: ','2pm: ','3pm: ','4pm: ','5pm: ','6pm: ','7pm: ','Total:'];
+var storeArray = [storeOne, storeTwo, storeThree, storeFour, storeFive];
+var parentElement = document.getElementById("table");
+var listArr = [];
+var ItemArr = [];
+
+for (var x = 0; x < 5; x++) {
+    listArr[x] = document.createElement("ul");    
+    parentElement.appendChild(listArr[x]);
+    listArr[x].textContent = storeArray[x].storeLocation;
+    var ItemArr = [];
+    for (var index = 0; index < 15; index++) {
+        ItemArr[index] = document.createElement('li');
+        ItemArr[index].textContent = workingHours[index] + storeArray[x].hourlySales[index];
+        listArr[x].appendChild(ItemArr[index]);
+        if (index== 14){
+            ItemArr[index].textContent= workingHours[index] + storeArray[x].total;
+            listArr[x].appendChild(ItemArr[index]);
+        }
+    }
+    
+};
